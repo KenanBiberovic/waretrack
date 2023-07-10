c7ab258f72ca824463d83cbe9be1e047477fe712
Kenan Biberovic <kenanbiberovic020@gmail.com>
2023-07-10 21:00:26 +0200

    commit

--- src/Components/Firebase-config.js
+++ src/Components/Firebase-config.js
@@ -3,7 +3,7 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDs-j5HOTDvekRIwmUSz1EFvln7mKaRnkU",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "waretrack-39c38.firebaseapp.com",
   projectId: "waretrack-39c38",
   storageBucket: "waretrack-39c38.appspot.com",
